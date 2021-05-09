31e3b6eb71731a2c530255ac4bfff6df02a2f11c
Renovamen <renovamenzxh@gmail.com>
2021-05-09 19:20:56 +0800

    fix: windows will not be allowed to be dragged totally outside the screen

--- src/components/dock/Dock.js
+++ src/components/dock/Dock.js
@@ -22,7 +22,7 @@ export default function Dock({
 
   return (
     <div
-      className="dock w-full fixed bottom-0 overflow-x-scroll sm:overflow-x-visible"
+      className="dock w-full sm:w-max fixed left-0 right-0 mx-auto bottom-0 overflow-x-scroll sm:overflow-x-visible"
       style={{
         zIndex: hide ? 0 : 99999
       }}
