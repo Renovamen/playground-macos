6431ac7e90a7d927928c585ad9877e97e3663f1a
邹笑寒 <renovamenzxh@gmail.com>
2023-09-22 11:47:30 -0400

    chore: bump deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,11 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: "module"
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
-  },
+  extends: "@renovamen/eslint-config-react",
+  ignorePatterns: ["node_modules/", "dist/"],
   rules: {
     "@typescript-eslint/ban-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/no-unescaped-entities": "off",
-    "react/no-unknown-property": "off"
-  },
-  ignorePatterns: ["node_modules/", "dist/"]
+    "@typescript-eslint/no-unused-vars": "off"
+  }
 };
