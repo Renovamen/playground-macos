7b44b8a2c312e882442cb38e5df7774d97e2fce9
邹笑寒 <renovamenzxh@gmail.com>
2024-05-02 01:39:54 -0400

    chore: bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 import { renovamen } from "@renovamen/eslint-config";
 
-export default renovamen({
+const configs = renovamen({
   files: ["**/*.ts", "**/*.tsx"],
   rules: {
     "@typescript-eslint/ban-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off"
+    "@typescript-eslint/no-unused-vars": "off",
+    "react/jsx-no-undef": "off"
   }
 });
+
+export default configs;
